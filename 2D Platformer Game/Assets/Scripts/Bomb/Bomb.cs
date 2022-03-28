e7e47fa352d79f4c7c5617b688d1cbe104558f6b
xisigui <andreiulupon@gmail.com>
2022-03-29 05:18:13 +0800

    Player Movement & Hit Animation
    + Left and Right Player Movement
    + Hit Animation

--- 2D Platformer Game/Assets/Scripts/Bomb/Bomb.cs
+++ 2D Platformer Game/Assets/Scripts/Bomb/Bomb.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class Bomb : MonoBehaviour
 {
     [SerializeField] private Animator _anim;
-    public float timer = 2;
     public bool hasExploded = false;
+    public float timer = 2;
     public GameObject explosionEffect;
+    public float blastRadius = 5f;
+    public CameraShake CameraShake;
 
     // Start is called before the first frame update
     void Start()
@@ -21,15 +23,49 @@ public class Bomb : MonoBehaviour
         timer -= Time.deltaTime;
         if(timer <= 0f && !hasExploded)
         {
-            Explode();
+            Explode();       
+            CheckArea();    
+            CameraShake.Shake(0.1f, .1f);   
             hasExploded = true;
         }
     }
 
     void Explode()
     {
-        float timeScale = 0.45f;
-        Destroy( Instantiate(explosionEffect, transform.position, Quaternion.identity), timeScale);
-        Destroy(gameObject);
+       float timeScale = 0.45f;
+       Destroy(Instantiate(explosionEffect, transform.position, Quaternion.identity), timeScale);
+       Destroy(gameObject);
+    }
+
+    void CheckArea()
+    {      
+        Vector3 explosionPos = transform.position;
+        Collider2D[] objsInRange = Physics2D.OverlapCircleAll(explosionPos, blastRadius);
+        foreach (Collider2D blah in objsInRange)
+        {
+            //Debug.Log("Player hit" + blah.tag);
+            Vector2 impulse = blah.transform.position - this.transform.position;
+            Rigidbody2D rb = blah.GetComponent<Rigidbody2D>();
+            var kk = blah.GetComponent<PlayerController>();
+            var ani = blah.GetComponent<Animator>();        
+            if(rb != null) 
+            {
+                if(blah.tag != "Untagged")
+                {
+                    ani.SetTrigger("Hit");       
+                    // kk.TakeDamage(20);
+                    rb.AddForce(impulse * 6, ForceMode2D.Impulse);
+                }else
+                {
+                    rb.AddForce(impulse * 6, ForceMode2D.Impulse);
+                }       
+            }            
+        }
+    }
+
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(this.transform.position, blastRadius);
     }
 }
